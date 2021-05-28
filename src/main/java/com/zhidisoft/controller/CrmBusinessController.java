06f63a9118cef07d49bb69a4020a584d84b3084a
finutaelerc <sunxj12491639@163.com>
2021-05-28 20:40:32 +0800

    修改

--- src/main/java/com/zhidisoft/controller/CrmBusinessController.java
+++ src/main/java/com/zhidisoft/controller/CrmBusinessController.java
@@ -1,5 +1,36 @@
 package com.zhidisoft.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.zhidisoft.entity.CrmBusiness;
+import com.zhidisoft.mapper.CrmBusinessMapper;
+import com.zhidisoft.vo.CrmBusinessVo;
+
+@RequestMapping("/bus")
+@Controller
 public class CrmBusinessController {
 
+	@Autowired
+	CrmBusinessMapper crmBusinessMapper;
+	
+	@GetMapping("update/{id}")
+	public ModelAndView updateUI(@PathVariable("id") Long id){
+		ModelAndView mav = new ModelAndView("user/form","user",crmBusinessMapper.selectByPrimaryKey(id));
+		mav.addObject("method","update");
+		return mav;
+	}
+	
+	@PostMapping("update/{id}")
+	public ModelAndView update(@PathVariable("id") Long id,CrmBusinessVo vo){
+		CrmBusiness cb = vo;
+		crmBusinessMapper.updateByPrimaryKeySelective(cb);
+		return new ModelAndView("redirect:/user");
+	}
+	
 }
---
+++ src/main/java/com/zhidisoft/vo/CrmBusinessVo.java
@@ -0,0 +1,7 @@
+package com.zhidisoft.vo;
+
+import com.zhidisoft.entity.CrmBusiness;
+
+public class CrmBusinessVo extends CrmBusiness {
+
+}
